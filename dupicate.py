1e4074289ef3e47fbd7a7330b90d9dbe2a114c9a
Deepti-20 <80585331+Deepti-20@users.noreply.github.com>
2021-03-13 19:09:05 +0530

    Add files via upload

---
+++ dupicate.py
@@ -0,0 +1,20 @@
+
+    n=[19,17,12,17,18,10,17,14,12,13,11]
+    index=0
+    list=[]
+    list=[]
+    while index<(len(n):
+        if n[index]not in list:
+            list.append(n[index])
+            j=0
+            count=0
+            while j<len(n):
+                if n[index]==n[j]:
+                    count=count+1
+                j=j+1
+            if n[index] not in list1 and count>1:
+                list1.append(n[index])
+        indwx=index+1
+    print(list)
+    print(list1)
+        
